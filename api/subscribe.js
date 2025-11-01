3411710773b117888f4f45bea913a0b960825a8c
Tommy <ying.hahn@gmail.com>
2025-11-01 11:49:16 +0900

    to mail

--- api/subscribe.js
+++ api/subscribe.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
     // 向你的邮箱发送通知
     await resend.emails.send({
       from: 'no-reply@myu-api.vercel.app',
-      to: process.env.TO_EMAIL,
+      to: email,
       subject: '新的订阅请求',
       html: `<p>新的订阅邮箱: <strong>${email}</strong></p>`,
     });
